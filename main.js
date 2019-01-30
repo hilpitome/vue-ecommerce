c1a119853f2ea8a5ca048408f51c56a6f463e9ff
hilpitome <hilpitome@gmail.com>
2019-01-30 16:02:45 +0300

    looping through complex collections

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'boots',
+        inStock:true,
         image:'./assets/socks.png',
-        details:["gender nuetrol", "80% cotton", "20% polyester"]
+        details:["gender nuetrol", "80% cotton", "20% polyester"],
+        variants:[
+            {
+                variantId:120,
+                variantColor:"blue"
+            },
+            {
+                variantId:122,
+                variantColor:"greed"
+            }
+        ]
     }
 });
