554c84a2c649e08a7ff0eeec508029b7a7e3cf29
hilpitome <hilpitome@gmail.com>
2019-02-01 15:27:30 +0300

    computed properties

--- main.js
+++ main.js
@@ -1,44 +1,101 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'boots',
-        brand:'hlnation',
-        selectedVariant:0,
-        details:["gender nuetrol", "80% cotton", "20% polyester"],
-        variants:[
-            {
-                variantId:120,
-                variantColor:"blue",
-                variantImage:'./assets/blue-socks.jpeg',
-                variantQuantity: 10
-            },
-            {
-                variantId:122,
-                variantColor:"green",
-                variantImage:'./assets/green-socks.png',
-                variantQuantity: 0
+Vue.component('product',{
+    template:" <div class=\"product\">\n" +
+        "         <div class=\"product-image\">\n" +
+        "             <img :src=\"image\" height=\"150\" width=\"120\">\n" +
+        "         </div>\n" +
+        "         <div class=\"product-info\">\n" +
+        "             <div v-for=\"(variant, index) in variants\"\n" +
+        "                  :key=\"variant.variantId\"\n" +
+        "                  class=\"color-box\"\n" +
+        "                  @mouseover=\"updateProduct(index)\"\n" +
+        "                  :style=\"{backgroundColor:variant.variantColor}\"\n" +
+        "             >\n" +
+        "\n" +
+        "             </div>\n" +
+        "             <h1>{{title}}</h1>\n" +
+        "             <p v-if=\"instock\">In stock</p>\n" +
+        "             <p v-else>Out of stock</p>\n" +
+        "             <p>shipping {{shipping}}</p> \n" +
+        "             <ul>\n" +
+        "                 <li v-for=\"detail in details\">{{detail}}</li>\n" +
+        "             </ul>\n" +
+        "\n" +
+        "             <div>\n" +
+        "                 <button v-on:click=\"addToCart\" :disabled=\"!instock\">Add to cart</button>\n" +
+        "             </div>\n" +
+        "             <div>\n" +
+        "                 <p>Cart ({{cart}})</p>\n" +
+        "             </div>\n" +
+        "         </div>\n" +
+        "         \n" +
+        "     </div>",
+        props:{
+            premium:{
+                type: Boolean,
+                required:false
             }
-        ],
-        cart:0
-    },
-    methods:{
-        addToCart: function(){
-            this.cart +=1;
         },
-        updateProduct: function(index){
-            this.selectedVariant = index;
-            console.log(index)
+        data(){
+        return {
+            product: 'boots',
+            brand:'hlnation',
+            selectedVariant:0,
+            details:["gender nuetrol", "80% cotton", "20% polyester"],
+            variants:[
+                {
+                    variantId:120,
+                    variantColor:"blue",
+                    variantImage:'./assets/blue-socks.jpeg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId:122,
+                    variantColor:"green",
+                    variantImage:'./assets/green-socks.png',
+                    variantQuantity: 0
+                }
+            ],
+            cart:0
         }
     },
-    computed:{
-        title(){
-            return this.brand+' '+this.product+" here";
+        methods:{
+            addToCart: function(){
+                this.cart +=1;
+            },
+            updateProduct: function(index){
+                this.selectedVariant = index;
+                console.log(index)
+            }
         },
-        image(){
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        instock(){
-            return this.variants[this.selectedVariant].variantQuantity;
+        computed:{
+            title(){
+                return this.brand+' '+this.product+" here";
+            },
+            image(){
+                return this.variants[this.selectedVariant].variantImage;
+            },
+            instock(){
+                return this.variants[this.selectedVariant].variantQuantity;
+            },
+            shipping(){
+                if(this.premium) {
+                    return "free";
+                }
+
+                    return '$2.99'
+
+            }
         }
     }
-});
+    );
+
+
+
+
+
+var app = new Vue({
+    el: '#app',
+    data:{
+        premium:false
+    }
+})
